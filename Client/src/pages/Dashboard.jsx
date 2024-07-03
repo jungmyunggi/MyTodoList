46ad95d534aece5227f5aca09925ad5d63adcefa
jungmyunggi <143400940+jungmyunggi@users.noreply.github.com>
2024-07-04 00:34:21 +0900

    fix: Dashboard 버그 수정
    다른 컴포넌트를 불러오지 못하던 버그 수정

--- Client/src/pages/Dashboard.jsx
+++ Client/src/pages/Dashboard.jsx
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from '../components/Nav';
+import Home from '../components/Views/Home';
+import Todo from '../components/Views/Todo';
+import MyPage from '../components/Views/Mypage';
 export default function Dashboard() {
-    return <Nav />;
+    const [activeSection, setActiveSection] = useState(1);
+
+    const renderSection = () => {
+        switch (activeSection) {
+            case 1:
+                return <Todo />;
+            case 2:
+                return <Home />;
+            case 3:
+                return <MyPage />;
+            default:
+                return null;
+        }
+    };
+
+    return (
+        <div className='dashboard-container'>
+            <header className='dashboard-header'></header>
+            <nav className='dashboard-nav'>
+                <Nav onSelectSection={setActiveSection} />
+            </nav>
+            <section className='dashboard-section'>
+                {renderSection()}
+            </section>
+            <footer className='dashboard-footer'></footer>
+        </div>
+    );
 }
