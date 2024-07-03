1778a9fd024baae6b93a2e856e53815c3d76c5f5
jungmyunggi <143400940+jungmyunggi@users.noreply.github.com>
2024-07-04 00:30:33 +0900

    fix: formatDate 로직변경
    새로운 파일로 formatDate.js를 생성하여 여러군데서 사용가능하도록 수정

--- Client/src/components/Clock.jsx
+++ Client/src/components/Clock.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import formatDate from '../util/formatDate';
 export default function Clock({ classname }) {
     const [time, setTime] = useState(new Date());
 
@@ -9,20 +10,13 @@ export default function Clock({ classname }) {
         return () => clearInterval(intervalId);
     }, []);
 
-    const formatDate = (date) => {
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const day = String(date.getDate()).padStart(2, '0');
-        const hour = String(date.getHours()).padStart(2, '0');
-        const minute = String(date.getMinutes()).padStart(2, '0');
-        const section = String(date.getSeconds()).padStart(2, '0');
-
-        return `${year}년${month}월${day}일 ${hour}시${minute}분${section}초`;
-    };
+    
     return (
         <div className={classname} style={{ textAlign: 'center' }}>
-            <h1>현재 시간</h1>
-            <h1>{formatDate(time)}</h1>
+          
+            <h1>{formatDate(time).slice(0,11)}</h1>
+            <h1>{formatDate(time).slice(11)}</h1>
+            
         </div>
     );
 }
